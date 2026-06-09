be29a5a485eb88db786053f25b557de23e3c64c7
karadocDebian <frederic.gobfert@gmail.com>
2026-06-09 11:08:15 +0200

    feat(web): ajout du header et navbar

--- web/src/main.inc.php
+++ web/src/main.inc.php
@@ -4,3 +4,22 @@ session_start();
 
 require_once __DIR__ . '/config/config.php';
 
+/**
+ * Permet de savoir si l'utilisateur est connecté.
+ * générique pour l'instant.
+ * TODO coorriger avec token reçu par l'API.
+ */
+function isUserLoggedIn(): bool
+{
+    return true;
+    // return isset($_SESSION['user']);
+}
+
+/**
+ * Permet de simplifier les liens.
+ */
+function url(string $path): string
+{
+    return BASE_URL . '/' . ltrim($path, '/');
+}
+
