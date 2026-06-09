490b871d89fb30630a917bc5e91c44ddfbe8e6c3
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-06-09 15:19:29 +0200

    feat: add city controller and router with endpoints for retrieving cities and their events

--- api/src/city/city.router.ts
+++ api/src/city/city.router.ts
@@ -1,3 +1,9 @@
-import { Router } from 'express';
+import {Router} from 'express';
+import {getCities, getCityById, getEventByCity} from "./city.controller.js";
 
 export const cityRouter = Router();
+
+//routes
+cityRouter.get('/', getCities)
+cityRouter.get('/:cityId', getCityById)
+cityRouter.get('/:cityId/events', getEventByCity)
