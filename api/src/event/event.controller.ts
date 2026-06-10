e1d7c734e72718a39dccb1fa4c84f6f4bef44a54
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-06-10 16:14:28 +0200

    feat(event): implement CRUD operations for events with JWT verification

--- api/src/event/event.controller.ts
+++ api/src/event/event.controller.ts
@@ -23,9 +23,9 @@ export const getEvents = async (_req: Request, res: Response) => {
 /**
  * Récupérer un événement en particulier par son id
  */
-export const getCityById = async (req: Request, res: Response) => {
+export const getEventById = async (req: Request, res: Response) => {
     // On récupère l'id
-    const eventId = req.params.eventId
+    const eventId = req.params.id
     try {
         //On regarde si l'événement existe
         const event = await prisma.event.findUnique({
@@ -57,7 +57,7 @@ export const getCityById = async (req: Request, res: Response) => {
  */
 export const getReviewsByEvent = async (req: Request, res: Response) => {
     // on récupère l'id de l'événement
-    const eventId = req.params.eventId
+    const eventId = req.params.id
     try {
         //On regarde si l'événement existe
         const event = await prisma.event.findUnique({
@@ -101,7 +101,7 @@ export const getReviewsByEvent = async (req: Request, res: Response) => {
 /**
  * Permet de créer un événement
  */
-/*export const postEvent = async (req: Request, res: Response) => {
+export const postEvent = async (req: Request, res: Response) => {
     // On récupère les données du body (json)
     const {
         type,
@@ -112,7 +112,6 @@ export const getReviewsByEvent = async (req: Request, res: Response) => {
         nbGuests,
         title,
         weather,
-        imageUrl,
         FK_cityId
     } = req.body
 
@@ -121,11 +120,156 @@ export const getReviewsByEvent = async (req: Request, res: Response) => {
             res.status(400).json({
                 message: "Certains champs obligatoire sont manquants pour la création d'un événement"
             })
+            return
         }
-        const eventTitle = await prisma.event.
+        //on vérifie si la ville existe
+        const city = await prisma.city.findUnique({
+            where: {
+                id: FK_cityId
+            }
+        })
+        if (!city) {
+            res.status(400).json({
+                message: `La ville avec l'id '${FK_cityId} n'existe pas`
+            })
+            return
+        }
+
+        //On récupère l'utilisateur
+        const userId = req.userId
+
+        const eventToCreate = await prisma.event.create({
+            data: {
+                type: type,
+                startDate: startDate,
+                endDate: endDate,
+                description: description,
+                isOutdoor: isOutdoor,
+                nbGuests: nbGuests,
+                title: title,
+                weather: weather,
+                FK_cityId: FK_cityId,
+                createdBy: userId
+            }
+        })
+
+        res.status(201).json({
+            message: 'Événement créé avec succès.',
+            result: eventToCreate
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Une erreur est survenue lors de la création de l'événement.",
+            error: error
+        })
     }
-}*/
+}
 
 //##### PUT #####
+/**
+ * Permet de modifier un événement
+ */
+export const putEvent = async (req: Request, res: Response) => {
+    const eventId = req.params.id
+    //On récupère les données du body
+    const {
+        type,
+        startDate,
+        endDate,
+        description,
+        isOutdoor,
+        nbGuests,
+        title,
+        weather,
+        FK_cityId
+    } = req.body
+
+    try {
+        //On vérifie si l'event existe
+        const event = prisma.event.findUnique({
+            where: {
+                id: eventId
+            }
+        })
+        if (!event) {
+            res.status(404).json({
+                message: `Événement avec l'id '${eventId}' introuvable.`
+            })
+            return
+        }
+        //On vérifie si la ville existe
+        const city = prisma.city.findUnique({
+            where: {
+                id: FK_cityId
+            }
+        })
+        if (!city) {
+            res.status(404).json({
+                message: `Ville avec l'id '${FK_cityId}' introuvable.`
+            })
+            return
+        }
+
+        const eventToUpdate = await prisma.event.update({
+            where: {
+                id: eventId
+            },
+            data: {
+                type: type,
+                startDate: startDate,
+                endDate: endDate,
+                description: description,
+                isOutdoor: isOutdoor,
+                nbGuests: nbGuests,
+                title: title,
+                weather: weather,
+                FK_cityId: FK_cityId,
+            }
+        })
+        res.status(200).json({
+            message: `Événement '${title}' avec l'id '${eventId}' mis à jour.`,
+            event: eventToUpdate
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Erreur lors de la modification de l'événement",
+            error: error
+        })
+    }
+}
 
 //##### DELETE #####
+/**
+ * Permet de supprimer un événement
+ */
+export const deleteEvent = async (req: Request, res: Response) => {
+    const eventId = req.params.id
+    try {
+        const event = await prisma.event.findUnique({
+            where: {
+                id: eventId
+            }
+        })
+        if (!event) {
+            res.status(404).json({
+                message: `Événement avec l'id '${eventId}' introuvable.`
+            })
+            return
+        } else {
+            const eventToDelete = await prisma.event.delete({
+                where: {
+                    id: eventId
+                }
+            })
+            res.status(200).json({
+                message: `Événement '${eventToDelete.title}' supprimé.`,
+                event: eventToDelete
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Erreur lors de la suppression de l'événement",
+            error: error
+        })
+    }
+}
--- api/src/event/event.router.ts
+++ api/src/event/event.router.ts
@@ -1,3 +1,13 @@
-import { Router } from 'express'; 
+import { Router } from 'express';
+import { verifyJWT } from '../common/jwt.middleware.js';
+import {getEvents, getReviewsByEvent, postEvent, getEventById, putEvent, deleteEvent} from './event.controller.js';
 
 export const eventRouter = Router();
+
+//routes
+eventRouter.get('/', getEvents)
+eventRouter.get('/:id', getEventById)
+eventRouter.get('/:id/reviews', getReviewsByEvent)
+eventRouter.post('/', verifyJWT, postEvent)
+eventRouter.put('/:id', verifyJWT, putEvent)
+eventRouter.delete('/:id', verifyJWT, deleteEvent)
