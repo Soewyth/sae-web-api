0ae978eb76f52d323b2be393d87cd777517bbcea
Whiplashhh <willem.vanbaelinghem@proton.me>
2026-06-12 10:50:31 +0200

    feat(event): add endpoint to retrieve event types

--- api/src/event/event.router.ts
+++ api/src/event/event.router.ts
@@ -1,15 +1,24 @@
 import { Router } from 'express';
 import { verifyJWT } from '../common/jwt.middleware.js';
-import {getEvents, getReviewsByEvent, postEvent, getEventById, putEvent, deleteEvent} from './event.controller.js';
+import {
+  getEvents,
+  getEventTypes,
+  getReviewsByEvent,
+  postEvent,
+  getEventById,
+  putEvent,
+  deleteEvent,
+} from './event.controller.js';
 import { postReview } from '../review/review.controller.js';
 
 export const eventRouter = Router();
 
 //routes
-eventRouter.get('/', getEvents)
-eventRouter.get('/:id', getEventById)
-eventRouter.get('/:id/reviews', getReviewsByEvent)
-eventRouter.post('/', verifyJWT, postEvent)
-eventRouter.put('/:id', verifyJWT, putEvent)
-eventRouter.delete('/:id', verifyJWT, deleteEvent)
-eventRouter.post('/:id/review', verifyJWT, postReview)
+eventRouter.get('/', getEvents);
+eventRouter.get('/type', getEventTypes);
+eventRouter.get('/:id', getEventById);
+eventRouter.get('/:id/reviews', getReviewsByEvent);
+eventRouter.post('/', verifyJWT, postEvent);
+eventRouter.put('/:id', verifyJWT, putEvent);
+eventRouter.delete('/:id', verifyJWT, deleteEvent);
+eventRouter.post('/:id/review', verifyJWT, postReview);
